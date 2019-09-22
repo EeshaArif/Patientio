43b4351c024d7b5ab294ba0a464386cece1b759e
EeshaArif <eeshaarif@gmail.com>
2019-09-22 20:53:16 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Patientio
+ getting started
---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\eesha\dev\d_project\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
